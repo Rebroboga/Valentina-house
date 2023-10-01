6068bae1cd901541411db888597e6cf0a5de55e0
Rebroboga <Rebroboga10@mail.ru>
2023-10-01 19:38:13 +0300

    add new pages

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  effect: "fade",
+})
+
